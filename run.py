cd6dbf76ee2fe2818b22ab5c6c1af82994a11add
Suth272 <suth272@outlook.com>
2024-06-21 23:22:14 +0100

    FIX: Fixed an error that didn't process the user input and update the grid.

--- run.py
+++ run.py
@@ -1,108 +1,110 @@
 import random
+
 class Boards:
-    """
-    Creates boards for the User and the Computer.
-    """
-    def __init__(self, board):
-        self.board = board
+  """
+  Creates boards for the User and the Computer.
+  """
+  def __init__(self, board):
+    self.board = board
 
-    def translate_letters_to_numbers():
-        """
-        Translates the column labels so the computer can read it.
-        """
-        letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6}
-        return letters_to_numbers
+  def translate_letters_to_numbers():
+    """
+    Translates the column labels so the computer can read it.
+    """
+    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6,}
+    return letters_to_numbers
 
-    def print_board(self):
-        """        
-        Prints the board that the user guesses on.
-        """
-        print("  A B C D E F G")
-        print(" ---------------")
-        row_number = 1
-        for row in self.board:
-            print("%d|%s|" % (row_number, "|".join(row)))
-            row_number += 1
+  def print_board(self):
+    """
+    Prints the board that the user guesses on.
+    """
+    print("  A B C D E F G")
+    print("  +-+-+-+-+-+-+")
+    row_number = 1
+    for row in self.board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 class Ships:
+  """
+  This class handles the placement of the ships by the computer, handles the user's inputs on their chosen co-ordinates, as well as counting the number of ships that were hit.
+  """
+  def __init__(self, board):
+    self.board = board
+
+  def create_ships(self):
     """
-    This class handles the placement of the ships by the computer, handles the user's inputs on their chosen co-ordinates, as well as counting the number of ships that were hit.
+    Creates 7 random ships for the Computer board.
     """
-    def __init__(self, board):
-        self.board = board
+    for i in range(5):
+      self.x_row, self.y_column = random.randint(0, 6), random.randint(0, 6)
+      while self.board[self.x_row][self.y_column] == "X":
+        self.x_row, self.y_column = random.randint(0, 6), random.randint(0, 6)
+      self.board[self.x_row][self.y_column] = "X"
+    return self.board
 
-    def create_ships(self):
-        """
-        Creates 7 random ships for the Computer board.
-        """
-        for i in range(7):
-            self.x_row, self.y_column = random.randint(0, 6), random.randint(0, 6)
-            
-        while self.board[self.x_row][self.y_column] == "X":
-            self.x_row, self.y_column = random.randint(0, 6), random.randint(0, 6)
-            self.board[self.x_row][self.y_column] = "X"
-            return self.board
+  def user_input(self):
+    """
+    Gets the user's input of their desired row and column of the ship, and provides user friendly error messages to prevent crashes.
+    """
+    try:
+      x_row = input("Enter the row of the ship: ")
+      while x_row not in '1234567':
+          print('Not an appropriate choice, please select a valid row')
+          x_row = input("Enter the row of the ship: ")
 
-    def user_input(self):
-        """
-        Gets the user's input of their desired row and column of the ship, and provides user friendly error messages to prevent crashes.
-        """
-        try:
-            x_row = input("Enter the row of the ship: ")
-            while x_row not in '1234567':
-                print('Please select a number between 1 and 7 for a valid row.')
-                x_row = input("Enter the row of the ship: ")
+      y_column = input("Enter the column letter of the ship: ").upper()
+      while y_column not in "ABCDEFG":
+          print('Not an appropriate choice, please select a valid column')
+          y_column = input("Enter the column letter of the ship: ").upper()
+      return int(x_row) - 1, Boards.translate_letters_to_numbers()[y_column]
+    except ValueError and KeyError:
+      print("Not a valid input")
+      return self.user_input()
 
-            y_column = input("Enter the column letter of the ship: ").upper()
-            while y_column not in "ABCDEFG":
-                print('Please select a letter from A, B, C, D, E, F, and G for a valid column.')
-                y_column = input("Enter the column letter of the ship: ").upper()
-                return int(x_row) - 1, Boards.translate_letters_to_numbers()[y_column]
-        except ValueError and KeyError:
-            print("Invalid input")
-            return self.user_input()
-
-    def count_hits(self):
-        """
-        Counts the number of the ships on the board that have been hit.
-        """
-        hit_ships = 0
-        for row in self.board:
-            for column in row:
-                if column == "X":
-                    hit_ships += 1
-        return hit_ships
+  def count_hits(self):
+    """
+    Counts the number of the ships on the board that have been hit.
+    """
+    hit_ships = 0
+    for row in self.board:
+      for column in row:
+        if column == "X":
+          hit_ships += 1
+    return hit_ships
 
 def PlayGame():
-    """
-    This function starts and loops the game till the user runs out of turns.
-    """ 
-    computer_board = Boards([[" "] * 7 for i in range(7)])
-    user_board = Boards([[" "] * 7 for i in range(7)])
-    Ships.create_ships(computer_board)
-    turns = 20
-    while turns > 0:
-        Boards.print_board(user_board)
-        user_x_row, user_y_column = Ships.user_input(object)
-    
-    while user_board.board[user_x_row][user_y_column] == "O" or user_board.board[user_x_row][user_y_column] == "X":
-        print("You have guessed this co-ordinate already.")
-        user_x_row, user_y_column = Ships.user_input(object)
-        if computer_board.board[user_x_row][user_y_column] == "X":
-            print("Direct hit! Enemy battleship sunk!")
-            user_board.board[user_x_row][user_y_column] = "X"
-        else:
-            print("Miss! Select new co-ordinates and try again.")
-            user_board.board[user_x_row][user_y_column] = "O"
-        
-        if Ships.count_hits(user_board) == 7:
-            print("You hit all 7 ships! You win!")
-            break
-        elif turns == 0:
-            print("You ran out of shells(turns). You lost!")
-            Boards.print_board(user_board)
-            break
-        else:
-            turns -= 1
-            print(f"You have {turns} turns remaining.")
+  """
+  This function starts and loops the game till the user runs out of turns.
+  """ 
+  computer_board = Boards([[" "] * 7 for i in range(7)])
+  user_guess_board = Boards([[" "] * 7 for i in range(7)])
+  Ships.create_ships(computer_board)
+  turns = 10
+  while turns > 0:
+    Boards.print_board(user_guess_board)
+    #get user input
+    user_x_row, user_y_column = Ships.user_input(object)
+    #check if duplicate guess
+    while user_guess_board.board[user_x_row][user_y_column] == "O" or user_guess_board.board[user_x_row][user_y_column] == "X":
+      print("You have guessed this co-ordinate already.")
+      user_x_row, user_y_column = Ships.user_input(object)
+    #check for hit or miss
+    if computer_board.board[user_x_row][user_y_column] == "X":
+      print("Direct hit! Enemy battleship sunk!")
+      user_guess_board.board[user_x_row][user_y_column] = "X"
+    else:
+      print("Miss! Select new co-ordinates and try again.")
+      user_guess_board.board[user_x_row][user_y_column] = "O"
+    #check for win or lose
+    if Ships.count_hits(user_guess_board) == 5:
+      print("You hit all 5 battleships! You win!")
+      break
+    elif turns == 0:
+        print("You ran out of shells(turns). You lost!")
+        Boards.print_board(user_guess_board)
+        break
+    else:
+      turns -= 1
+      print(f"You have {turns} turns remaining.")
 
 PlayGame()
