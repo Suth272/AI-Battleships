d2d336bc1ca967b509ee60d0f69c37f11280540d
Suth272 <suth272@outlook.com>
2024-06-22 00:26:24 +0100

    FEAT: Adjusted print statements for better interactivity between game/code and the user. Also made the grid size smaller to make the games quicker.

--- run.py
+++ run.py
@@ -11,15 +11,15 @@ class Boards:
     """
     Translates the column labels so the computer can read it.
     """
-    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6,}
+    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
     return letters_to_numbers
 
   def print_board(self):
     """
     Prints the board that the user guesses on.
     """
-    print("  A B C D E F G")
-    print("  +-+-+-+-+-+-+")
+    print("  A B C D E")
+    print(" -----------")
     row_number = 1
     for row in self.board:
         print("%d|%s|" % (row_number, "|".join(row)))
@@ -33,12 +33,12 @@ class Ships:
 
   def create_ships(self):
     """
-    Creates 7 random ships for the Computer board.
+    Creates 5 random ships for the Computer board.
     """
     for i in range(5):
-      self.x_row, self.y_column = random.randint(0, 6), random.randint(0, 6)
+      self.x_row, self.y_column = random.randint(0, 4), random.randint(0, 4)
       while self.board[self.x_row][self.y_column] == "X":
-        self.x_row, self.y_column = random.randint(0, 6), random.randint(0, 6)
+        self.x_row, self.y_column = random.randint(0, 4), random.randint(0, 4)
       self.board[self.x_row][self.y_column] = "X"
     return self.board
 
@@ -47,16 +47,16 @@ class Ships:
     Gets the user's input of their desired row and column of the ship, and provides user friendly error messages to prevent crashes.
     """
     try:
-      x_row = input("Enter the row of the ship: ")
-      while x_row not in '1234567':
-          print('Not an appropriate choice, please select a valid row')
-          x_row = input("Enter the row of the ship: ")
+      x = input("Enter the row of the ship: ")
+      while x not in '12345':
+          print('Not an appropriate choice, please select a valid row between 1 and 5.')
+          x = input("Enter the row of the ship: ")
 
-      y_column = input("Enter the column letter of the ship: ").upper()
-      while y_column not in "ABCDEFG":
-          print('Not an appropriate choice, please select a valid column')
-          y_column = input("Enter the column letter of the ship: ").upper()
-      return int(x_row) - 1, Boards.translate_letters_to_numbers()[y_column]
+      y = input("Enter the column letter of the ship: ").upper()
+      while y not in "ABCDE":
+          print('Not an appropriate choice, please select a valid column from A, B, C, D or E.')
+          y = input("Enter the column letter of the ship: ").upper()
+      return int(x) - 1, Boards.translate_letters_to_numbers()[y]
     except ValueError and KeyError:
       print("Not a valid input")
       return self.user_input()
@@ -76,32 +76,36 @@ def PlayGame():
   """
   This function starts and loops the game till the user runs out of turns.
   """ 
-  computer_board = Boards([[" "] * 7 for i in range(7)])
-  user_guess_board = Boards([[" "] * 7 for i in range(7)])
+  computer_board = Boards([[" "] * 5 for i in range(5)])
+  user_board = Boards([[" "] * 5 for i in range(5)])
   Ships.create_ships(computer_board)
   turns = 10
   while turns > 0:
-    Boards.print_board(user_guess_board)
-    #get user input
-    user_x_row, user_y_column = Ships.user_input(object)
-    #check if duplicate guess
-    while user_guess_board.board[user_x_row][user_y_column] == "O" or user_guess_board.board[user_x_row][user_y_column] == "X":
-      print("You have guessed this co-ordinate already.")
-      user_x_row, user_y_column = Ships.user_input(object)
-    #check for hit or miss
-    if computer_board.board[user_x_row][user_y_column] == "X":
+    Boards.print_board(user_board)
+    user_x, user_y = Ships.user_input(object)
+    
+    """
+    Grid checking and changing/appending.
+    """  
+    if computer_board.board[user_x][user_y] == "X":
       print("Direct hit! Enemy battleship sunk!")
-      user_guess_board.board[user_x_row][user_y_column] = "X"
+      user_board.board[user_x][user_y] = "X"
     else:
       print("Miss! Select new co-ordinates and try again.")
-      user_guess_board.board[user_x_row][user_y_column] = "O"
-    #check for win or lose
-    if Ships.count_hits(user_guess_board) == 5:
+      user_board.board[user_x][user_y] = "O"
+    while user_board.board[user_x][user_y] == "O" or user_board.board[user_x][user_y] == "X":
+      print("You have guessed this co-ordinate already.")
+      user_x, user_y = Ships.user_input(object)
+      
+    """
+    Checking for win and lose conditons.
+    """
+    if Ships.count_hits(user_board) == 5:
       print("You hit all 5 battleships! You win!")
       break
     elif turns == 0:
         print("You ran out of shells(turns). You lost!")
-        Boards.print_board(user_guess_board)
+        Boards.print_board(user_board)
         break
     else:
       turns -= 1
